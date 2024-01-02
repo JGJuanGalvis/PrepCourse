26447a04515b2e232011e6ced7cd90da8732c957
JGJuanGalvis <juan.gaalvis@gmail.com>
2024-01-02 10:37:20 -0500

    Modulo 07 Ejercicio 01 terminado

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -34,18 +34,20 @@ function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
-   class usuario{
-   saludar (){
+  Usuario.prototype.saludar = function(){
+   return "Hello World!";
+  };
 
-      return "Hello World!";
-   }
-}
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+
+   String.prototype.reverse = function(){
+     return this.split('').reverse().join('');
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
